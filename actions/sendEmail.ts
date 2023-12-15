8b95076ceb52b372018383bbd4ae73e3627acf41
Jeremyjdz13 <46872022+Jeremyjdz13@users.noreply.github.com>
2023-12-15 11:38:02 -0800

    Removed use server in actions

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import React from 'react'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async (formData: FormData) => {
-    "use server"
+    
     const senderEmail = formData.get('senderEmail')
     const senderMessage = formData.get('message')
 
